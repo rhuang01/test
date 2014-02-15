18ede3cf029acc430b7635a0e4249a94985ff536
rhuang01 <richard.y.huang@gmail.com>
2014-02-15 21:25:46 +0000

    new pic

---
+++ db/migrate/20140215203824_create_pics.rb
@@ -0,0 +1,14 @@
+class CreatePics < ActiveRecord::Migration
+  def change
+    create_table :pics do |t|
+
+    	t.string :title
+    	t.text :message
+    	t.string :who
+
+
+      t.timestamps
+    end
+  end
+
+end
